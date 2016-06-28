6b158218d9c4e9a27bd363237c51729c66f054bd
Nathan John <nj1407@gmail.com>
2016-06-28 12:55:32 -0500

    updated interface for door_open

--- src/door_open.cpp
+++ src/door_open.cpp
@@ -72,6 +72,10 @@
 #include <time.h>
 #include <stdint.h>
 
+#include <segbot_arm_manipulation/arm_utils.h>
+
+#define NUM_JOINTS 8
+
 Eigen::Vector4f centroid;
 
 typedef pcl::PointXYZRGB PointT;
@@ -80,12 +84,57 @@ typedef pcl::PointCloud<PointT> PointCloudT;
 PointCloudT::Ptr cloud (new PointCloudT);
 geometry_msgs::PoseStamped first_goal;
 geometry_msgs::PoseStamped second_goal;
+sensor_msgs::JointState joint_state_outofview;
+sensor_msgs::JointState current_state;
+geometry_msgs::PoseStamped current_pose;
+bool heardPose = false;
+bool heardJoinstState = false;
+
+void joint_state_cb (const sensor_msgs::JointStateConstPtr& input) {
+	
+	if (input->position.size() == NUM_JOINTS){
+		current_state = *input;
+		heardJoinstState = true;
+	}
+  //ROS_INFO_STREAM(current_state);
+}
+
+
+void toolpos_cb (const geometry_msgs::PoseStamped &msg) {
+  current_pose = msg;
+  heardPose = true;
+  //  ROS_INFO_STREAM(current_pose);
+}
+
+
+// Blocking call for user input
+void pressEnter(){
+	std::cout << "Press the ENTER key to continue";
+	while (std::cin.get() != '\n')
+		std::cout << "Please press ENTER\n";
+}
+
+void listenForArmData(float rate){
+	heardPose = false;
+	heardJoinstState = false;
+	ros::Rate r(rate);
+	
+	while (ros::ok()){
+		ros::spinOnce();
+		
+		if (heardPose && heardJoinstState)
+			return;
+		
+		r.sleep();
+	}
+}
 
 void goal_cb (const geometry_msgs::PoseStampedConstPtr& input)
 {
 		ROS_INFO("entered goal_cb");
 		first_goal.header = input->header;
 		first_goal.pose = input->pose; 
+		
 }
 
 void goal_cb_2 (const geometry_msgs::PoseStampedConstPtr& input)
@@ -101,22 +150,38 @@ int main (int argc, char** argv)
 	ros::init (argc, argv, "segbot_arm_door_open_detector");
 	ros::NodeHandle n;
 	
-	//create subscriber to joint angles
-	//ros::Subscriber sub_angles = n.subscribe ("/joint_states", 1, joint_state_cb);
+
 	
+	//create subscriber to joint angles
+	ros::Subscriber sub_angles = n.subscribe ("/joint_states", 1, joint_state_cb);
+	
+	//subsrcibe to goals
 	ros::Subscriber goal_sub = n.subscribe ("goal_to_go", 1,goal_cb);
 
 	ros::Subscriber goal_sub_2 = n.subscribe ("goal_to_go_2", 1,goal_cb_2);
+	
+	//create subscriber to tool position topic
+	ros::Subscriber sub_tool = n.subscribe("/mico_arm_driver/out/tool_position", 1, toolpos_cb);
+	
 	//service
 	
 	ros::ServiceClient client = n.serviceClient<door_manipulation_demo::door_perception>("/door_handle_detection/door_perception");
 	
 	ros::ServiceClient client_move = n.serviceClient<moveit_utils::MicoMoveitCartesianPose>("mico_cartesianpose_service");
 	
-
+	//get arm position
+	segbot_arm_manipulation::closeHand();
+	listenForArmData(30.0);
+	joint_state_outofview = current_state;
+	
+	//set goal for mico service
 	door_manipulation_demo::door_perception door_srv;
 	moveit_utils::MicoMoveitCartesianPose mico_srv;
 	mico_srv.request.target = first_goal;
+	
+
+	
+
 	
 	//make calls to get vision
 	if(client.call(door_srv)){
@@ -128,6 +193,10 @@ int main (int argc, char** argv)
 	} else {
 		ROS_INFO("didn't enter");
 	}		
+	pressEnter();
+    ROS_INFO("Demo starting...Move the arm to a 'ready' position .");
+	segbot_arm_manipulation::homeArm(n);
+	
 	ros::spinOnce();
 	if(client_move.call(mico_srv)){
 		ROS_INFO(" entered srv move 1 ");
@@ -135,7 +204,7 @@ int main (int argc, char** argv)
 		ROS_INFO("didn't entered srv move 1 ");
 	}
 	
-	mico_srv.request.target = second_goal;
+	/*mico_srv.request.target = second_goal;
 	//goal_pose.position.y += .1;
 	ros::spinOnce();
 	if(client_move.call(mico_srv)){
@@ -143,10 +212,13 @@ int main (int argc, char** argv)
 	} else {
 		ROS_INFO("didn't entered srv move 2 ");
 	}
+	*/
 	
+	//ros::spinOnce();
+	pressEnter();
+    ROS_INFO("Demo ending...arm will move back 'ready' position .");
+	segbot_arm_manipulation::moveToJointState(n,joint_state_outofview);
 	
-	
-	ros::spinOnce();
 	//refresh rate
 	double ros_rate = 3.0;
 	ros::Rate r(ros_rate);
